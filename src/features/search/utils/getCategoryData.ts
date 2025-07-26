2460f5cfe6d32226b70c41ad4aa255015b245c8e
K-jisu <wltn7star@naver.com>
2025-07-26 21:45:22 +0900

    fix: getCategoryData 함수의 반환 타입 수정 및 기본 아이콘 처리 개선

--- src/features/search/utils/getCategoryData.ts
+++ src/features/search/utils/getCategoryData.ts
@@ -3,12 +3,12 @@ import { searchCategories } from '../constants/search-category-constants';
 /**
  * 카테고리의 아이콘과 색상을 반환하는 함수
  * @param label - 카테고리의 라벨
- * @returns { icon: string, color: string }
+ * @returns { icon: Component, color: string }
  */
 export const getCategoryData = (label: string) => {
   const foundCategory = searchCategories.find((category) => category.label === label);
   return {
-    icon: foundCategory?.icon ?? '❓',
+    icon: foundCategory?.icon,
     color: foundCategory?.color ?? 'foggyBlue',
   };
 };
